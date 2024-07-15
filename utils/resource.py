6c772bb0a5bd5306adbd9b4bb0b6813397877413
Maksim Zosimov <mizo-y@yandex.ru>
2024-07-15 18:21:33 +0200

    еще раз исправлено Ч.2 high-level-step-objects

--- utils/resource.py
+++ utils/resource.py
@@ -2,6 +2,7 @@ import os
 
 
 def path(value):
-    current_directory = os.path.dirname(os.path.realpath(__file__))
-    return str(os.path.abspath(os.path.join(current_directory, value)))
-
+    current_directory = os.getcwd()
+    parent_directory = os.path.dirname(current_directory)
+    image_directory = os.path.join(parent_directory, 'resources')
+    return str(os.path.abspath(os.path.join(image_directory, value)))
