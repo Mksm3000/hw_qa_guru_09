6e1104d14ad65003ddfcc3f9f77b8bc5843496d6
Maksim Zosimov <mizo-y@yandex.ru>
2024-07-15 18:23:40 +0200

    еще раз исправлено Ч.2 high-level-step-objects

--- utils/resource.py
+++ utils/resource.py
@@ -6,3 +6,4 @@ def path(value):
     parent_directory = os.path.dirname(current_directory)
     image_directory = os.path.join(parent_directory, 'resources')
     return str(os.path.abspath(os.path.join(image_directory, value)))
+
