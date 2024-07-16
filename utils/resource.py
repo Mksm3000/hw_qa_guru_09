fb0fc05163bf26e5d1137905bcbf9899b5b999e3
Maksim Zosimov <mizo-y@yandex.ru>
2024-07-16 20:06:42 +0200

    исправлен def path в resource.py  Ч.2 high-level-step-objects

--- utils/resource.py
+++ utils/resource.py
@@ -2,8 +2,7 @@ import os
 
 
 def path(value):
-    current_directory = os.getcwd()
-    parent_directory = os.path.dirname(current_directory)
-    image_directory = os.path.join(parent_directory, 'resources')
-    return str(os.path.abspath(os.path.join(image_directory, value)))
-
+    starting_path = os.path.abspath(__file__)
+    project_root_directory = os.path.join(starting_path, os.pardir, os.pardir)
+    image_directory = os.path.join(project_root_directory, 'resources')
+    return os.path.abspath(os.path.join(image_directory, value))
